e65340eb7dc6fab9d2a27f66c0b8f85f312df653
IVIanuu <IVIanuu@gmail.com>
2018-09-30 20:08:57 +0200

    Use local maven

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -8,6 +8,7 @@ object Versions {
     const val targetSdk = 28
     const val versionCode = 1
     const val versionName = "0.0.1"
+    const val groupId = "com.github.IVIanuu.director"
 
     const val androidGradlePlugin = "3.2.0"
 
