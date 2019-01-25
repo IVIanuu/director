a9bfec82a6f62627e83eca4efa6e05f572a4c694
IVIanuu <IVIanuu@gmail.com>
2019-01-25 12:03:21 +0100

    hmm

--- sample/src/main/java/com/ivianuu/director/sample/app/App.kt
+++ sample/src/main/java/com/ivianuu/director/sample/app/App.kt
@@ -25,7 +25,7 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        //if (LeakCanary.isInAnalyzerProcess(this)) return
+        if (LeakCanary.isInAnalyzerProcess(this)) return
         //LeakCanary.install(this)
     }
 
