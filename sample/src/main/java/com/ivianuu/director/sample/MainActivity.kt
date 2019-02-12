920c61c278fcf604d6e284b0419a6a3a1ab63dca
IVIanuu <IVIanuu@gmail.com>
2019-02-12 12:56:49 +0100

    Removed sample test

--- sample/src/main/java/com/ivianuu/director/sample/MainActivity.kt
+++ sample/src/main/java/com/ivianuu/director/sample/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.ivianuu.director.sample
 
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import com.ivianuu.director.changeHandler
-import com.ivianuu.director.common.changehandler.FadeChangeHandler
 import com.ivianuu.director.fragmenthost.getRouter
 import com.ivianuu.director.hasRoot
-import com.ivianuu.director.sample.controller.DragDismissController
 import com.ivianuu.director.sample.controller.HomeController
-import com.ivianuu.director.sample.controller.SimpleDialogController
+import com.ivianuu.director.setRoot
 import com.ivianuu.director.toTransaction
 import kotlinx.android.synthetic.main.activity_main.controller_container
 import kotlinx.android.synthetic.main.activity_main.toolbar
@@ -29,30 +25,8 @@ class MainActivity : AppCompatActivity(), ActionBarProvider {
 
         with(getRouter(controller_container)) {
             if (!hasRoot) {
-//                setRoot(HomeController().toTransaction())
+                setRoot(HomeController().toTransaction())
             }
-
-            setBackstack(
-                listOf(
-                    HomeController().toTransaction()
-                        .changeHandler(FadeChangeHandler()),
-                    DragDismissController().toTransaction()
-                        .changeHandler(FadeChangeHandler(removesFromViewOnPush = true)),
-                    SimpleDialogController().toTransaction()
-                        .changeHandler(FadeChangeHandler(removesFromViewOnPush = false))
-                ), true
-            )
-
-            Handler().postDelayed(Runnable {
-                setBackstack(
-                    listOf(
-                        backstack[0],
-                        backstack[1].controller.toTransaction()
-                            .changeHandler(FadeChangeHandler(removesFromViewOnPush = false)),
-                        backstack[2]
-                    ), true
-                )
-            }, 2000)
         }
     }
 
