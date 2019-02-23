4b37219ee5684a29d9c131c0a16fed0ef88bccd2
IVIanuu <IVIanuu@gmail.com>
2019-02-23 14:04:00 +0100

    Version bump

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -15,7 +15,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.director"
     const val vcsUrl = "https://github.com/IVIanuu/director"
-    const val version = "${Build.versionName}-dev-10"
+    const val version = "${Build.versionName}-dev-11"
 }
 
 object Versions {
